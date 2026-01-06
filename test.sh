eb2f8923c3b6469a01145f35a808e1da59bec889
Angel Penchev <C-apenchev@cccis.com>
2026-01-06 12:14:12 +0200

    fix: update tail function test script for improved output validation

--- test.sh
+++ test.sh
@@ -1,22 +1,27 @@
+#!/bin/bash
+function run_test {
+    echo $1
+    EXPECTED_OUTPUT=$($1)
+    EXPECTED_STATUS=$?
+    ACTUAL_OUTPUT=$($2)
+    ACTUAL_STATUS=$?
+
+    if [ "$EXPECTED_STATUS" -ne "$ACTUAL_STATUS" ]; then
+        diff  <(echo "$EXPECTED_STATUS") <(echo "$ACTUAL_STATUS")
+        echo "Status Failure"
+        TEST_RESULT=$(($TEST_RESULT + 1))
+    fi
+
+    if [ "$EXPECTED_OUTPUT" != "$ACTUAL_OUTPUT" ]; then
+        diff  <(echo "$EXPECTED_OUTPUT") <(echo "$ACTUAL_OUTPUT")
+        echo "Output Failure"
+        TEST_RESULT=$(expr "$TEST_RESULT + 1")
+    fi
+}
 make
-
-EXPECTED_OUTPUT=$(tail input.txt)
-EXPECTED_STATUS=$?
-ACTUAL_OUTPUT=$(./tail input.txt)
-ACTUAL_STATUS=$?
-
 TEST_RESULT=0
 
-if [ "$EXPECTED_STATUS" -ne "$ACTUAL_STATUS" ]; then
-    diff  <(echo "$EXPECTED_STATUS") <(echo "$ACTUAL_STATUS")
-    echo "Status Failure"
-    TEST_RESULT=1
-fi
-
-if [ "$EXPECTED_OUTPUT" != "$ACTUAL_OUTPUT" ]; then
-    diff  <(echo "$EXPECTED_OUTPUT") <(echo "$ACTUAL_OUTPUT")
-    echo "Output Failure"
-    TEST_RESULT=1
-fi
+run_test "./tail input.txt" "tail input.txt"
+run_test "./tail non-existent.txt" "tail non-existent.txt"
 
 exit $TEST_RESULT
